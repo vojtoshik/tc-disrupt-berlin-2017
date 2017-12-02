5f46fd7372742bf87423cde2cccbaa36fdfe3c0c
Oleksandr Shchetynin <oleksandr.shchetynin@gmail.com>
2017-12-02 14:27:11 +0200

    initial repo commit

---
+++ src/main/java/com/tc/hoodwatch/HoodwatchApplication.java
@@ -0,0 +1,12 @@
+package com.tc.hoodwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoodwatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoodwatchApplication.class, args);
+	}
+}
